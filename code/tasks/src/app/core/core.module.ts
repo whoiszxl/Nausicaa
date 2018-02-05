e7068f84d14a06048e2d099bbc2609b298fbd6e9
zhouxiaolong <zhouxl@lamezhi.com>
2018-02-05 17:46:58 +0800

    整体布局实现

--- code/tasks/src/app/core/core.module.ts
+++ code/tasks/src/app/core/core.module.ts
@@ -1,13 +1,21 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
 })
-export class CoreModule { 
+export class CoreModule {
 
   //使用SkipSelf,使用的时候默认不会在当前类里面寻找，不然构造函数的判断会一直卡死在当前类中
   //第一次加载不存在CoreModule，使用@Optional注解让其是可选的，如果有就判断，没有就直接构造
